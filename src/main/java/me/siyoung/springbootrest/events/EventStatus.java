ca0948071b83571b58ece6b1606f64a54c0ac250
KIMSIYOUNG <tldud2404@gmail.com>
2019-09-30 21:34:04 +0900

    Add create evenets test

--- src/main/java/me/siyoung/springbootrest/events/EventStatus.java
+++ src/main/java/me/siyoung/springbootrest/events/EventStatus.java
@@ -1,4 +1,6 @@
 package me.siyoung.springbootrest.events;
 
 public enum EventStatus {
+
+    DRAFT, PUBLISHED, BEGAN_ENROLLMENT;
 }
--- src/test/java/me/siyoung/springbootrest/events/EventTest.java
+++ src/test/java/me/siyoung/springbootrest/events/EventTest.java
@@ -1,5 +1,31 @@
-import static org.junit.Assert.*;
+package me.siyoung.springbootrest.events;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class EventTest {
 
+    @Test
+    public void builder(){
+        Event event = Event.builder()
+                .name("siyoung spring rest api")
+                .description("rest api development with spring")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean(){
+        String name ="event";
+        String description = "test event";
+
+        Event event = new Event();
+        event.setName("event");
+        event.setDescription("test event");
+
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
 }
