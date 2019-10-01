14d17d00e57c1a0b954f5501ca53434bd39badcf
KIMSIYOUNG <tldud2404@gmail.com>
2019-10-01 11:29:03 +0900

    Add bad request body by adding Errors to JsonComponent

--- src/test/java/me/siyoung/springbootrest/events/EventControllerTests.java
+++ src/test/java/me/siyoung/springbootrest/events/EventControllerTests.java
@@ -2,6 +2,7 @@ package me.siyoung.springbootrest.events;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import me.siyoung.springbootrest.common.TestDescription;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -33,6 +34,7 @@ public class EventControllerTests {
 
 
     @Test
+    @TestDescription("정상적으로 동작하는 이벤트 등록")
     public void createEvent() throws Exception {
         EventDto event = EventDto.builder()
                 .name("spring")
@@ -63,22 +65,9 @@ public class EventControllerTests {
         ;
     }
     @Test
+    @TestDescription("입력값이 비어있을 때 에러가 나는 테스")
     public void Bad_Request() throws Exception {
-        Event event = Event.builder()
-                .id(100)
-                .name("spring")
-                .description("rest api start")
-                .beginEnrollmentDateTime(LocalDateTime.of(2019,11,23,14,15))
-                .closeEnrollmentDateTime(LocalDateTime.of(2019,11,24,14,15))
-                .beginEventDateTime(LocalDateTime.of(2019,11,23,14,15))
-                .endEventDateTime(LocalDateTime.of(2019,11,30,23,59))
-                .basePrice(100)
-                .maxPrice(200)
-                .limitOfEnrollment(100)
-                .location("강남역 D2")
-                .free(true)
-                .offline(true)
-                .build();
+        Event event = Event.builder().build();
         mockMvc.perform(post("/api/events/")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .accept(MediaTypes.HAL_JSON)
@@ -90,6 +79,7 @@ public class EventControllerTests {
     }
 
     @Test
+    @TestDescription("입력값이 잘못되어 있는 경우 발생하는 테스트")
     public void CreateEvent_Bad_Request_Empty_Input() throws Exception {
         EventDto eventDto = EventDto.builder()
                 .name("spring")
@@ -107,7 +97,12 @@ public class EventControllerTests {
         mockMvc.perform(post("/api/events/")
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .content(this.objectMapper.writeValueAsString(eventDto)))
-                .andExpect(status().isBadRequest());
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$[0].objectName").exists())
+                .andExpect(jsonPath("$[0].defaultMessage").exists())
+                .andExpect(jsonPath("$[0].code").exists())
+        ;
 
     }
 
