ac8d22f7bdcbfa680587033921c7e332ca8bf37b
KIMSIYOUNG <tldud2404@gmail.com>
2019-10-01 09:16:33 +0900

    event repository

--- src/test/java/me/siyoung/springbootrest/events/EventControllerTests.java
+++ src/test/java/me/siyoung/springbootrest/events/EventControllerTests.java
@@ -1,17 +1,23 @@
 package me.siyoung.springbootrest.events;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.time.LocalDateTime;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -21,14 +27,37 @@ public class EventControllerTests {
 
     @Autowired
     MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+    @MockBean
+    EventRepository eventRepository;
 
     @Test
     public void createEvent() throws Exception {
-        mockMvc.perform(post("/api/events")
+        Event event = Event.builder()
+                .name("spring")
+                .description("rest api start")
+                .beginEnrollmentDateTime(LocalDateTime.of(2019,11,23,14,15))
+                .closeEnrollmentDateTime(LocalDateTime.of(2019,11,24,14,15))
+                .beginEventDateTime(LocalDateTime.of(2019,11,23,14,15))
+                .endEventDateTime(LocalDateTime.of(2019,11,30,23,59))
+                .basePrice(100)
+                .maxPrice(200)
+                .limitOfEnrollment(100)
+                .location("강남역 D2")
+                .build();
+
+        event.setId(11);
+        Mockito.when(eventRepository.save(event)).thenReturn(event);
+
+        mockMvc.perform(post("/api/events/")
                     .contentType(MediaType.APPLICATION_JSON_UTF8)
                     .accept(MediaTypes.HAL_JSON)
-                        )
-                .andExpect(status().isCreated());
+                    .content(objectMapper.writeValueAsString(event)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("id").exists())
+        ;
     }
 
 
