37aedf8fb38ed6088875b75c7b0b85bdd05c3226
KIMSIYOUNG <tldud2404@gmail.com>
2019-10-01 12:04:55 +0900

    Refactory check test by JunitParams

--- src/test/java/me/siyoung/springbootrest/events/EventTest.java
+++ src/test/java/me/siyoung/springbootrest/events/EventTest.java
@@ -1,11 +1,14 @@
 package me.siyoung.springbootrest.events;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import me.siyoung.springbootrest.common.TestDescription;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class EventTest {
 
     @Test
@@ -34,57 +37,47 @@ public class EventTest {
 
     @Test
     @TestDescription("비지니스 로직에 따라 무료/유료를 구분하는지 테스트")
-    public void free(){
+    @Parameters
+    public void free(int basePrice, int maxPrice, boolean isFree){
         //given
         Event event = Event.builder()
-                .basePrice(0)
-                .maxPrice(0)
+                .basePrice(basePrice)
+                .maxPrice(maxPrice)
                 .build();
         //when
         event.updated();
         //then
-        assertThat(event.isFree()).isTrue();
+        assertThat(event.isFree()).isEqualTo(isFree);
 
-        //given
-        event = Event.builder()
-                .basePrice(100)
-                .maxPrice(0)
-                .build();
-        //when
-        event.updated();
-        //then
-        assertThat(event.isFree()).isFalse();
-
-        //given
-        event = Event.builder()
-                .basePrice(0)
-                .maxPrice(100)
-                .build();
-        //when
-        event.updated();
-        //then
-        assertThat(event.isFree()).isFalse();
     }
+    public Object[] parametersForFree(){
+        return new Object[]{
+                new Object[]{0,0,true},
+                new Object[]{0,100,false},
+                new Object[]{100,0,false},
+                new Object[]{100,200,false}
+        };
+    }
+
     @Test
     @TestDescription("위치 정보에 따라 오프라인과 온라인 유뮤의 구분")
-    public void location(){
+    @Parameters
+    public void location(String location, boolean isOffline){
         //given
         Event event = Event.builder()
-                .location("강남역 5번출구")
+                .location(location)
                 .build();
         //when
         event.updated();
         //then
-        assertThat(event.isOffline()).isTrue();
-
-        //given
-        event = Event.builder()
-                .build();
-        //when
-        event.updated();
-        //then
-        assertThat(event.isOffline()).isFalse();
-
+        assertThat(event.isOffline()).isEqualTo(isOffline);
+    }
+    public Object[] parametersForLocation(){
+        return new Object[]{
+                new Object[]{"강남",true},
+                new Object[]{null,false},
+                new Object[]{"          ",false}
+        };
     }
 
 }
